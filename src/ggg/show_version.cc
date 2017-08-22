66bdb284bd36d411b2dee0997c88c9dc735169a2
Ivan Gankevich <igankevich@ya.ru>
2017-08-22 10:23:44 +0300

    Show revision and build date in version subcommand.

--- src/ggg/show_version.cc
+++ src/ggg/show_version.cc
@@ -1,10 +1,47 @@
 #include "show_version.hh"
 
 #include <iostream>
+#include <unistd.h>
 
 #include "config.hh"
+#include "version.hh"
+#include "build_date.hh"
+
+void
+ggg::Show_version::parse_arguments(int argc, char* argv[])  {
+	int opt;
+	while ((opt = getopt(argc, argv, "rd")) != -1) {
+		if (opt == 'r') {
+			this->_field = Field::Revision;
+		} else if (opt == 'd') {
+			this->_field = Field::Date;
+		}
+	}
+}
 
 void
 ggg::Show_version::execute()  {
-	std::cout << GGG_VERSION "\n";
+	const char* field;
+	switch (this->_field) {
+		case Field::Version:
+			field = GGG_VERSION;
+			break;
+		case Field::Revision:
+			field = GGG_REVISION;
+			break;
+		case Field::Date:
+			field = GGG_BUILD_DATE;
+			break;
+		default:
+			field = "unknown";
+			break;
+	}
+	std::cout << field << '\n';
 }
+
+void
+ggg::Show_version::print_usage() {
+	std::cout << "usage: " GGG_EXECUTABLE_NAME " "
+		<< this->prefix() << " [-rd] COMMAND" << '\n';
+}
+
