aa07531e3aa0104e35fbee96cc68eb354c93f0b5
Ivan Gankevich <i.gankevich@spbu.ru>
2020-03-28 20:48:37 +0300

    Fix forms api.

--- src/ggg/guile/ggg/forms.scm
+++ src/ggg/guile/ggg/forms.scm
@@ -1,16 +1,16 @@
 (define-module (ggg forms))
 (use-modules (rnrs io ports)
-			 (ggg types))
+             (ggg types))
 
 (define (passwd-exists name)
   (catch #t
-		 (lambda () (getpwnam name))
-		 (lambda (key . parameters) #f)))
+    (lambda () (getpwnam name))
+    (lambda (key . parameters) #f)))
 
 (define (group-exists name)
   (catch #t
-		 (lambda () (getgrnam name))
-		 (lambda (key . parameters) #f)))
+    (lambda () (getgrnam name))
+    (lambda (key . parameters) #f)))
 
 (define-public (get-password-newline port)
   (let ((pw (get-password port)))
@@ -19,30 +19,30 @@
 (define-public (input-value prompt read validate)
   (put-string (current-output-port) prompt)
   (let ((value (read (current-input-port))))
-	(if (validate value) value (input-value prompt read validate))))
+    (if (validate value) value (input-value prompt read validate))))
 
 (define-public (input-regex prompt regex)
   (input-value prompt
-			   get-line
-			   (lambda (value) (regexp-exec regex value))))
+               get-line
+               (lambda (value) (regexp-exec regex value))))
 
 (define-public (input-password prompt entropy)
   (input-value prompt
-			   get-password-newline
-			   (lambda (value) (check-password value entropy))))
+               get-password-newline
+               (lambda (value) (check-password value entropy))))
 
 (define-public (input-username prompt regex)
   (input-value prompt
-			   get-line
-			   (lambda (value) (and (regexp-exec regex value)
-									(not (ggg-entity-exists value))
-									(not (passwd-exists value))))))
+               get-line
+               (lambda (value) (and (regexp-exec regex value)
+                                    (not (entity-exists value))
+                                    (not (passwd-exists value))))))
 
 (define-public (validate-username value regex)
   (and (regexp-exec regex value)
-	   (not (ggg-entity-exists value))
-	   (not (passwd-exists value))
-	   (not (group-exists value))))
+       (not (entity-exists value))
+       (not (passwd-exists value))
+       (not (group-exists value))))
 
 (define-public (validate-password value entropy)
-			   (check-password value entropy))
+  (check-password value entropy))
